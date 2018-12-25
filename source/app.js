b33a2ce134708d2d79c51c5574fcca01db78c7d6
Amir Tavakoli <amirtavakoli@outlook.com>
2018-12-26 00:03:46 +0330

    dumper added

--- source/app.js
+++ source/app.js
@@ -1,9 +1,18 @@
 var _ = require('underscore');
 var $ = require('jquery');
+const { dd } = require('dumper.js');
 require('backbone');
 var AppView = require('./app.view')
 $(function() {
   var view = new AppView({
     el: 'app'
   });
+  
+  const users = [
+    { user: 'barney', age: 36, active: true, createdAt: new Date(), getAge: () => this.age },
+    { user: 'fred', age: 40, active: false, createdAt: new Date(), getAge: () => this.age },
+    { user: 'pebbles', age: 1, active: true, createdAt: new Date(), getAge: () => this.age }
+  ];
+
+  dd(users);
 });
