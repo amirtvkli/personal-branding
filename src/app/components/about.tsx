010f5ce547f19292b94e6f91927a8251c6eab63b
Amir Tavakoli <amirtavakoli@outlook.com>
2019-02-16 00:16:27 +0330

    loading and catch-error are added to about page to handle un-wanted problems

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,27 +1,40 @@
 import * as React from 'react';
 import { Component } from 'react';
+declare var require: any;
+let loadingGif = require('assets/images/loading-gif.gif');
 
 
 class About extends Component{
-    
+    state:any;
     constructor(props?: any, context?: any){
         super(props,context);
+         this.state = {
+            posts: [],
+            media:'',
+            isLoading: false,
+            error:null
+        };
     }
     
-    state:any = {
-        posts: [],
-        media:''
-    };
+   
     
     componentDidMount(){
-        let Url = "http://amirtvkli.ir/wordpress/index.php/wp-json/wp/v2/pages?slug='about'&type='page'"
+        this.setState({isLoading:true});
+        let Url = "http://amirtvkli.ir/wordpress/index.php/wp-json/wp/v2/pages?slug='about'&type='page'";
+        
         fetch(Url)
         .then(response => response.json())
         .then(response =>{
             this.setState({
-                posts:response
+                posts:response,
+                isLoading:false
             })
         })
+        .catch(error => this.setState({
+            error,
+            isLoading: false
+        }));
+     
     }
     
     getImageUrlfromPost(post:any){
@@ -37,14 +50,35 @@ class About extends Component{
         })
     }
     render(){   
-        let aboutPageContents = this.state.posts.map((post:any,index:any)=>{
+        const { posts, media, isLoading, error } = this.state;
+
+        if (error) {
+            return (
+                <div className="row justify-content-center align-items-center">
+                    <div className="row justify-content-center align-items-center col-md-9 page__container page__content">
+                        <p>{error.message}</p>
+                    </div>
+                </div>  
+                );
+        }
+
+        if (isLoading) {
+            return  (
+                <div className="row justify-content-center align-items-center">
+                    <div className="row justify-content-center align-items-center col-md-9 page__container page__content">
+                        <img src={loadingGif}></img>
+                    </div>
+                </div>  
+            )
+        }
+
+        let aboutPageContents = posts.map((post:any,index:any)=>{
             let img = this.getImageUrlfromPost(post);
             this.getImageSrcfromUrl(img);
             return (
                 <div className="col-md-6 page__content" key={index}>
-                <h1>{post.title.rendered}</h1>
-                
-                <p>{post.content.rendered}</p>
+                    <h1>{post.title.rendered}</h1>
+                    <p>{post.content.rendered}</p>
                 </div>
                 )
             })
@@ -52,7 +86,7 @@ class About extends Component{
             <div className="row justify-content-center align-items-center">
                 <div className="row justify-content-center align-items-center col-md-9 page__container">
                     <div className="col-md-3">
-                        <img src={this.state.media}></img>
+                        <img src={media}></img>
                     </div>
                     {aboutPageContents}
                 </div>
